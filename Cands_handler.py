c67c21ab39c64ee59968cb51e9411b63114500fb
Vivek Gupta <gup037@athena.atnf.csiro.au>
2023-01-17 12:27:39 +1100

    Added clustering functions

--- Cands_handler.py
+++ Cands_handler.py
@@ -1,16 +1,23 @@
 import logging 
+from sklearn.cluster import DBSCAN
+import numpy as np
 
 class Cands_handler:
     '''
     To be implemented
     '''
-    def __init__(self, outname):
+    def __init__(self, outname, clustering_eps = 3):
         self.outname = outname
+        self.header_inkeys = ['SNR', 'boxcar', 'DM', 'samp', 'ngroup']
+        self.header_outkeys = self.header_inkeys + ['ncluster']
+        self.db = DBSCAN(eps=clustering_eps, min_samples=1)
         self.open_outfile()
 
     def open_outfile(self):
         self.f = open(self.outname, 'w')
-        self.f.write("SNR\tboxcar\tDM\tsamp\tngroup\n")
+        for key in self.header_outkeys:
+            self.f.write(key + "\t")
+        self.f.write("\n")
     
     def write_cands(self, cands):
         if len(cands) > 0:
@@ -21,4 +28,33 @@ class Cands_handler:
                 self.f.write("\n")
         else:
             logging.debug("No cands to write")
+
+    def find_representative_cands(self, cands_arr, labels):
+        best_cands = []
+        for label in labels:
+            label_mask = labels == label
+            this_label_cands = cands_arr[label_mask]
+            ncands = len(this_label_cands)
+            best_cand_idx = np.argmax(this_label_cands[:, 0])
+            best_cand = list(this_label_cands[best_cand_idx])
+            best_cand.append(ncands)
+            best_cands.append(best_cand)
+        return best_cands
+        
+
+    def cluster_cands(self, cands):
+        cands_arr = np.array(cands)
+        #print("------> Cands_arr given to the cluster_cans fx is ", cands_arr, cands_arr.shape)
+        normalised_cands = np.zeros((len(cands), 3))
+        normalised_cands[:, 0] = cands_arr[:, 1] / 4
+        normalised_cands[:, 1] = cands_arr[:, 2] / 10
+        normalised_cands[:, 2] = cands_arr[:, 3]
+
+        clusters = self.db.fit(normalised_cands)
+
+        repr_cands = self.find_representative_cands(cands_arr, clusters.labels_)
+        return repr_cands
+
+
+
         
