417ef5540c994fce2bea3bc5ed50a01a0f8bbee1
Vivek Gupta <gup037@athena.atnf.csiro.au>
2023-01-20 14:26:35 +1100

    Added a class to load npy files and changed the behaviour of yield dx to not yield the last block with nsamps < nt

--- Ingester.py
+++ Ingester.py
@@ -15,7 +15,8 @@ class LoadFilterbank(FilReader):
         for iblock, _, block in self.read_plan(nt, start, nsamps, skipback, verbose = False):
             if block.size != self.nchans * nt:
                 nt_this_block = block.size // self.nchans
-                logging.info(f"Block {iblock} only has {nt_this_block} samps, yielding a smaller block")
+                logging.info(f"Block {iblock} only has {nt_this_block} samps, ending the yield loop now")
+                break
             else:
                 nt_this_block = nt
 
@@ -26,3 +27,56 @@ class LoadFilterbank(FilReader):
             nsamps = self.tot_samples
         return self.read_block(start, nsamps)
 
+class LoadNumpy:
+    def __init__(self, filename:str):
+        self.filename = filename
+        self.data = np.load(self.filename)
+        if self.data.ndim != 2:
+            raise TypeError("The datafile does not contain a 2-D array")
+        self.nchans = self.data.shape[0]
+        self.fbottom = 1152.5
+        self.df = 1
+        self.tot_samples = self.data.shape[1]
+
+    def yield_block(self, nt:int=256, start:int = 0, nsamps:int = None, skipback:int =0):
+        last_samp_read = start
+        stop_samp = (nsamps - start) // nt * nt + start
+        iblock = 0
+        if nsamps is None:
+            nsamps = self.tot_samples - start
+        while True:
+            if last_samp_read >= start + nsamps or last_samp_read >= self.tot_samples:
+                logging.info("Reached the end of file.. exiting the yielding while loop")
+                break
+            start_samp = last_samp_read
+            if iblock > 0:
+                start_samp = last_samp_read - skipback
+
+            if start_samp + nt > stop_samp:
+                logging.info("The last block would have less than nt samples, so ending the yield loop now")
+                break
+
+            end_samp = start_samp + nt
+            block = self.data[:, start_samp:end_samp]
+
+            logging.debug(f"Yielding block {iblock} with shape = {block.shape}")
+            last_samp_read = end_samp
+            iblock += 1
+
+            yield block
+            
+    def get_block(self, start:int=0, nsamps:int=None):
+        if start > self.tot_samples:
+            raise ValueError("start > tot_nsamps")
+        if nsamps < 0:
+            raise ValueError("nsamps cannot be < 0, say None for all samps")
+        
+        if nsamps is None:
+            return self.data[:, start:]
+        else:
+            return self.data[:, start:start + nsamps]
+    
+
+
+
+        
