a89ccb389b7764ea20ccffe5ce9b38f1a85b5796
Vivek Gupta <gup037@athena.atnf.csiro.au>
2023-01-20 14:28:36 +1100

    Added cmdline option to show diagnostic plots, added a check for the file extension to use the corresponding loader, added timing log lines and changed the dm_boxcar_scaling factors to be multiplicative instead of divisive

--- search_cas_fil.py
+++ search_cas_fil.py
@@ -1,4 +1,5 @@
 from Ingester import LoadFilterbank as F
+from Ingester import LoadNumpy as LN
 from Preprocessor import Normalise, RFI_mitigator
 from Dedisperser import Dedisperser
 from Boxcarer import Boxcar_and_threshold
@@ -10,42 +11,56 @@ import matplotlib.pyplot as plt
 
 
 def run_search(fil_name, nt, max_dm, max_boxcar, threshold, candfile):
-    f = F(fil_name)
-    tot_nblocks = f.header.nsamples // nt + 1
+    if fil_name.endswith("fil"):
+        f = F(fil_name)
+    elif fil_name.endswith("npy"):
+        f = LN(fil_name)
+    else:
+        raise ValueError("Invalid data type provided")
+    tot_nblocks = f.tot_samples // nt  + 1 if f.tot_samples %nt >0 else f.tot_samples //nt
     norm = Normalise()
     rfi_m = RFI_mitigator(cleaning_chunk=128)
     dd = Dedisperser(fmin = f.fbottom, df = f.df, nf = f.nchans, max_dm=max_dm, nt = nt)
     bt = Boxcar_and_threshold(nt = nt, boxcar_history=np.zeros((max_dm, max_boxcar)))
-    dm_boxcar_norm_factors = dd.get_rms_normalising_factor(max_boxcar)
+    dm_boxcar_norm_factors = 1 / dd.get_rms_normalising_factor(max_boxcar)
+    #print("dm_boxcar_norm_factors are:", dm_boxcar_norm_factors)
     ch = Cands_handler(outname = candfile)
     all_cands = []
     max_cands_per_block = 1e6
     for iblock, block in enumerate(f.yield_block(nt)):
         start = time.time()
         logging.info(f"Processing block {iblock}")
-        #plt.figure()
-        #plt.imshow(block, aspect='auto', interpolation='None')
-        #plt.title("Raw block")
+        if args.plot:
+            plt.figure()
+            plt.imshow(block, aspect='auto', interpolation='None')
+            plt.title("Raw block")
 
         logging.debug(f"Normalising the block")
         normed_block = norm.mad_norm(block)
-        #plt.figure()
-        #plt.imshow(normed_block, aspect='auto', interpolation='None')
-        #plt.title("Normed block")
+        norm_time = time.time()
+        if args.plot:
+            plt.figure()
+            plt.imshow(normed_block, aspect='auto', interpolation='None')
+            plt.title("Normed block")
 
         logging.debug("Cleaning the block")
         cleaned_block = rfi_m.clean_block(normed_block)
-        #plt.figure()
-        #plt.imshow(cleaned_block, aspect='auto', interpolation='None')
-        #plt.title("Cleaned block")
+        cleaning_time = time.time()
+        if args.plot:
+            plt.figure()
+            plt.imshow(cleaned_block, aspect='auto', interpolation='None')
+            plt.title("Cleaned block")
 
         logging.debug("Dedispersing the block")
         dd_block = dd.get_full_DMT(cleaned_block)
-        #plt.figure()
-        #plt.imshow(dd_block, aspect='auto', interpolation='None')
+        disp_time = time.time()
+        if args.plot:
+            plt.figure()
+            plt.imshow(dd_block, aspect='auto', interpolation='None')
 
         logging.debug("Running boxcar and threshold")
         cands = bt.boxcar_and_threshold(dd_block, threshold=threshold, dm_boxcar_norm_factors=dm_boxcar_norm_factors, iblock = iblock)
+        bt_time = time.time()
         ncands = len(cands)
         logging.debug(f"Got {ncands} cands in block {iblock}.")
         if ncands > max_cands_per_block:
@@ -60,8 +75,11 @@ def run_search(fil_name, nt, max_dm, max_boxcar, threshold, candfile):
             ch.write_cands(repr_cands)
             all_cands = []
         end = time.time()
-        logging.debug(f"It took {end - start}s")
-    #plt.show()
+        logging.debug(f"It took a total of {end - start}s")
+        logging.debug(f"The breakdown of times is as follows - start = {start - start}, norm = {norm_time - start}, cleaning = {cleaning_time - norm_time}, dmt_time = {disp_time - cleaning_time}, boxcar_and_thresh = {bt_time - disp_time} ")
+
+        if args.plot:
+            plt.show()
 
     logging.info("Closing cand file")
     ch.f.close()
@@ -89,6 +107,7 @@ if __name__ == '__main__':
     a.add_argument("-T", type=float, help="S/N threshold to get candidates (def = 10)", default=10)
     a.add_argument("-C", type=str, help="Name of file to write cands to (def = <filname>.cands", default=None)
     a.add_argument("-log_level", type=str, help="Logging level - [CRITICAL/INFO/DEBUG] (def = DEBUG)", default="DEBUG")
+    a.add_argument("-plot", action='store_true', help="Plot the different stages of processing for each block?", default=False)
 
     args = a.parse_args()
     main(args)
