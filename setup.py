1287a65f2576759b87a5f760284777328f7320e5
Vivek Gupta <vivg269@gmail.com>
2023-03-24 10:56:16 +1100

    adding the setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+requirements = ['numpy', 'matplotlib', 'pandas', 'numba', 'iqrm', 'sigpyproc']
+
+setup(name = "caspy_search",
+        version='1.0',
+        description = "Package to search filterbank data for single pulses using FDMT and boxcar",
+        author = "Vivek Gupta",
+        author_email = "vivek.gupta@csiro.au",
+        install_requires = requirements,
+        python_requires = '>3.6',
+        entry_points = {'console_scripts': ['search_cas_fil=caspy_search.search_cas_fil:main']}
+        )
